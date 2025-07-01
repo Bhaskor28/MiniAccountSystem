169406f02e53c603693f02aaf0fedf1cd02c7aa0
Bhaskor <bhaskorcse520@gmail.com>
2025-07-01 20:20:36 +0600

    Implemented Chart of Accounts module using Stored Procedure

---
+++ Controllers/ChartOfAccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MiniAccountSystem.Models;
+using MiniAccountSystem.Services;
+
+namespace MiniAccountSystem.Controllers
+{
+    [Authorize(Roles = "Admin,Accountant")]
+    public class ChartOfAccountController : Controller
+    {
+        private readonly IAccountChartService _service;
+
+        public ChartOfAccountController(IAccountChartService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var list = await _service.GetAllAsync();
+            return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AccountChart model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _service.AddAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AccountChart model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _service.UpdateAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
