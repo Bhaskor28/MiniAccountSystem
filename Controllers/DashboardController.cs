12b1ab4452736dd559b5e2261c3e43ff69accbdb
Bhaskor Roy <112170792+Bhaskor28@users.noreply.github.com>
2025-07-04 19:04:59 +0600

    Added Dashboard (#6)

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniAccountSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DashboardController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var roles = await _userManager.GetRolesAsync(user);
+            var role = roles.FirstOrDefault();
+
+            return role switch
+            {
+                "Admin" => View("AdminDashboard"),
+                "Accountant" => View("AccountantDashboard"),
+                "Viewer" => View("ViewerDashboard"),
+                _ => RedirectToAction("AccessDenied", "Account")
+            };
+        }
+    }
+}
